1d0cff9ea55a617a9e8fc350f303e82d67dc13c2
Marcio Nicolau <marcionicolau@me.com>
2018-07-16 16:15:59 -0300

    App first step

--- app1.py
+++ app1.py
@@ -11,3 +11,27 @@ USERNAME_PASSWORD_PAIRS = [
 ]
 
 app = dash.Dash()
+auth = dash_auth.BasicAuth(app, USERNAME_PASSWORD_PAIRS)
+server = app.server()
+
+app.layout = html.Div([
+    dcc.RangeSlider(
+        id='range-slider',
+        min=-5,
+        max=6,
+        marks={i: str(i) for i in range(-5, 7)},
+        value=[-3, 4]
+    ),
+    html.H1(id='product')  # this is the output
+], style={'width': '50%'})
+
+
+@app.callback(
+    Output('product', 'children'),
+    [Input('range-slider', 'value')])
+def update_value(value_list):
+    return value_list[0]*value_list[1]
+
+
+if __name__ == '__main__':
+    app.run_server()
